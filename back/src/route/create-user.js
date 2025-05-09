18ba8dd412d447747d5a155ea9a1fb97921c5f81
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-05-09 01:23:37 +0000

    feat: add `view:database` script and install `bcrypt` to insert passwords

--- back/src/route/create-user.js
+++ back/src/route/create-user.js
@@ -1,10 +1,21 @@
 import prisma from "../database/prisma.js";
 import { server } from "../server.js";
+import bcrypt from "bcrypt";
 
 export async function createUser() {
-  server.post("/create-user", (req, res) => {
+  server.post("/create-user", async (req, res) => {
     const { username, email, password } = req.body;
-    console.log(username, email, password);
+    const saltRounds = 10;
+
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+
+    const userCreated = await prisma.user.create({
+      data: {
+        name: username,
+        email: email,
+        password: hashPassword,
+      },
+    });
 
     res.status(201).send("created");
   });
